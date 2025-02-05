3b440f5fcafbe37a50aea976fbab6d7e3072b72d
Ahntus-bilip <doananhtu238@gmail.com>
2025-02-05 09:08:00 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào bé iu, anh bé có điều muốn nói. </br> Em là người con gái tuyệt vời nhất với anh!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Cảm ơn bé iu nhìu nhaaa.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn em đã cho anh một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cho em. </br> Anh biết anh không được tốt và cũng chẳng tâm lý chút nào nhưng tớ có một trái tim chân thành và không ngừng yêu em. Em chính là một thế giới nhỏ của anh. </br> Hôm nay Valentine anh chúc bé luôn vui vẻ nha. Em mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em.</br> Love you <3",
 };
