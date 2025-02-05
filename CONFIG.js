360957f3ae6f98bbb9e9dc1a71ead857126111a6
Ahntus-bilip <doananhtu238@gmail.com>
2025-02-05 09:55:44 +0700

    Update CONFIG.js
    Gửi đến người anh yêu

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào bé iu, anh bé có điều muốn nói. </br> Em là người con gái tuyệt vời nhất với anh!",
+  titleHeader: "Chào bé iu, anh bé có điều muốn nói. </br> Em là người con gái trên cả tuyệt vời nhất với anh!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+    "Hãy ghi nhớ những điều anh nói một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
   buttonYes: "Em đồng ý",
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn bé iu nhìu nhaaa.",
   descriptionModar:
-    "Cảm ơn em đã cho anh một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cho em. </br> Anh biết anh không được tốt và cũng chẳng tâm lý chút nào nhưng tớ có một trái tim chân thành và không ngừng yêu em. Em chính là một thế giới nhỏ của anh. </br> Hôm nay Valentine anh chúc bé luôn vui vẻ nha. Em mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em.</br> Love you <3",
+    "Cảm ơn em đã cho anh một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cho em. </br> Anh biết anh không được tốt và cũng chẳng tâm lý chút nào nhưng tớ có một trái tim chân thành và không ngừng yêu em luôn luôn là như vậy. Em chính là một thế giới nhỏ của anh. </br> Hôm nay Valentine anh chúc bé luôn vui vẻ nha. Em mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em.</br> Love you <3",
 };
